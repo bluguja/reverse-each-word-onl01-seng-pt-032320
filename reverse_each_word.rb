f61093e6d2ba7a942186a0d367965fd217edad01
Bala Luguja <bluguja@gmail.com>
2020-03-26 13:08:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 # start code here
 
-def reverse_each_word(string)
-  array = string.split(" ") #turn string into an array
+def reverse_each_word(str)
+  array = str.split(" ") #turning the string into an array 
   test_array = []
   array.collect do|string| #iterate over the array
     test_array.push string.reverse #reverse each word in the array
